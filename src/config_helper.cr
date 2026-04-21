a810d37008830e74e4b0d13d56625230f16c2aff
ana v e <ana@ana-gardua>
2026-04-21 10:15:14 -0600

    Add more snarky messages

--- src/config_helper.cr
+++ src/config_helper.cr
@@ -17,34 +17,48 @@ module ConfigHelper
     messages: MotivationalMsgs?
   )
 
-#   alias MotivationalMsgs = Hash(String, Array(String))
-
   DEFAULT_MESSAGES =  {
     "new_pomodoro":  [
       "Enter to start",
       "Let's get ready to rumble!",
       "Another day, another pomodoro.",
-      "Did you hydrate or naw?"
+      "Did you hydrate or naw?",
+      "Five more minutes, mom!"
     ],
 
     "work":  [
-      "LOL you're working, loser!"
+      "LOL you're working, loser!",
+      "Are we there yet?",
+      "Go go go go!",
+      "You've got this bb!"
     ],
 
     "new_short_break":  [
-      "Ready for a break already?"
+      "Ready for a break already?",
+      "Phew! Time for a breather.",
+      "Another one down!",
+      "Yippeeeeee!"
     ],
 
     "short_break":  [
-      "Go grab some coffee or something"
+      "Go grab some coffee!",
+      "Oooooh big stretch!",
+      "Drink some water!",
+      "Stand up, do some squats or something.",
     ],
 
     "new_long_break":  [
-      "You've been working hard haven't you?"
+      "You've been working hard haven't you?",
+      "Do *not*, under any circumstances, doom scroll during your break or so help me!",
+      "Scribbily, griddily, bibbily, doo! I have a brand new long break for you!",
+      "Am I really making myself come up with another one of these? Come on now."
     ],
 
     "long_break":  [
-      "Have a break. You've earned it!"
+      "Have a break. You've earned it!",
+      "Remember to take care of yourself!",
+      "Time for a quick walk.",
+      "My boss makes a dollar..."
     ]
   }
 
